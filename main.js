26d5f35eae6bc8c188c6df2acee5fb654782909b
Berserk <dilmuradovid@gmail.com>
2024-12-04 17:45:14 +0500

    shit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Exercise ---1---
+
+// let str = 'Hello'
+// let res = ''
+// res = str.split("").join(", ")
+// console.log(res)
+
+// Exercise ---2---
+
+// let text = 'Nigga123we12@$4'
+// let res = 0
+// for (let i = 0; i < text.length; ++i) {
+// 	if (Number(text[i])) {
+// 		res = res + (+text[i])
+// 	}
+// }
+// console.log(res);
+
+// Exercise ---3---
+
+// let str = "hi"
+// let res = ""
+// for(let i =0 ; i < str.length; ++i ){
+//    res += str[i] + str[i]
+// }
+// console.log(res);
+
+// Exercise ---3---
+
+// let str = 'anna'
+// res = ''
+// for (let i = str.length - 1; i >= 0; --i) {
+// 	res += str[i]
+// }
+// if (str === res) {
+// 	console.log(true)
+// } else {
+// 	console.log(false)
+// }
+
+
